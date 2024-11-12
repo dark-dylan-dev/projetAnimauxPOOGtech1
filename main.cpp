1743de0cdddff076c62a32d2f062a554c8c2983e
dhollemaert <dhollemaert@GCFRPA046>
2024-11-12 10:57:47 +0100

    Update

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <vector>
+
 using namespace std;
 
 bool jour = true;
@@ -183,11 +184,11 @@ public:
             cout << endl;
             // Ajout de l'animal crée dans le vecteur animaux
             switch (choix) { // A changer mardi avec les noms des espèces (vide pour l'instant)
-            case 1: animaux.push_back(Loup(nomAnimal));  break; case 6: animaux.push_back(Chevre(nomAnimal)); break; case 11: break; case 16: break; case 21: break;
-            case 2: animaux.push_back(Lapin(nomAnimal)); break; case 7:  break; case 12: break; case 17: break; case 22: break;
-            case 3: animaux.push_back(Ours(nomAnimal));  break; case 8:  break; case 13: break; case 18: break; case 23: break;
-            case 4: animaux.push_back(Biche(nomAnimal)); break; case 9:  break; case 14: break; case 19: break; case 24: break;
-            case 5: animaux.push_back(Renard(nomAnimal)); break; case 10: break; case 15: break; case 20: break; case 25: break;
+            case 1: animaux.push_back(Loup(nomAnimal));   break; case 6: animaux.push_back(Chevre(nomAnimal)); break; case 11: break; case 16: break; case 21: break;
+            case 2: animaux.push_back(Lapin(nomAnimal));  break; case 7:                                       break; case 12: break; case 17: break; case 22: break;
+            case 3: animaux.push_back(Ours(nomAnimal));   break; case 8:                                       break; case 13: break; case 18: break; case 23: break;
+            case 4: animaux.push_back(Biche(nomAnimal));  break; case 9:                                       break; case 14: break; case 19: break; case 24: break;
+            case 5: animaux.push_back(Renard(nomAnimal)); break; case 10:                                      break; case 15: break; case 20: break; case 25: break;
             default: break;
             }
         }
