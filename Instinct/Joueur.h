f2e1f4af647df00d1a8628b24e8c90ec0edc87fc
Dylan Hollemaert <92250732+dark-dylan-93220@users.noreply.github.com>
2024-11-14 07:23:34 +0100

    Update Joueur.h

--- Instinct/Joueur.h
+++ Instinct/Joueur.h
@@ -8,11 +8,12 @@
 class Joueur
 {
 private:
-    string menu[7] = { "1. Nourrir", "2. Tuer", "3. Provoquer un conflit", "4. Reproduction", "5. Creer un animal", "6. Rapprocher deux animaux", "7. Quitter" };
+    string menu[8] = { "1. Nourrir", "2. Tuer", "3. Provoquer un conflit", "4. Reproduction", "5. Creer un animal", "6. Rapprocher deux animaux", "7. Ne rien faire", "8. Quitter"};
 public:
     void choixDebugMode(bool& debugMode);
     int afficherInfos(std::vector<Animal>& animaux) const;
     void afficherInfosSolo(std::vector<Animal>& animaux);
+    int WhileCinFail(unsigned int var, std::vector<Animal>& animaux);
     char choixJoueur(int choix, std::vector<Animal>& animaux);
 };
 
