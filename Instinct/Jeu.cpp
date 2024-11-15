f6d2efb0d8beabe78a228c3efd8e5d50645007de
Loan91 <loan.ravinpro@gmail.com>
2024-11-15 14:58:52 +0100

    Update Jeu.cpp

--- Instinct/Jeu.cpp
+++ Instinct/Jeu.cpp
@@ -97,8 +97,8 @@ void Jeu::BoucleDeJeu(vector<Animal>& animaux, int& tour, bool debug, Joueur& jo
         if (test == 1) { meteo = "Pluie"; }
         if (test == 2) { meteo = "Tempete"; }
         if (test == 3) { meteo = "Orage"; }
-        if (test == 4) { meteo = "nuageux"; }
-        if (test == 5) { meteo = "venteux"; }
+        if (test == 4) { meteo = "Nuageux"; }
+        if (test == 5) { meteo = "Venteux"; }
         // --- AFFICHAGES INFOS EN-TETE --- //
         cout << affichageTour << endl;
         cout << affichagePeriode << endl;
